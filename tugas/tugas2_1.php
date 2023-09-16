515111501997e15714bd3951be42439e1393fb5e
hiruma48 <49358000+hiruma48@users.noreply.github.com>
2023-09-16 07:16:04 +0700

    tugas 2 php
    mengumpulkan tugas php

---
+++ tugas/tugas2_1.php
@@ -0,0 +1,12 @@
+<?php 
+$waktu1 = 36 ;
+$jumlahTenagaKerja = 12;
+$jumlahTenagaKerja2 = 24;
+
+$jumlahpekerja = $jumlahTenagaKerja*$waktu1;
+$waktu2 = $jumlahpekerja/$jumlahTenagaKerja2;
+
+echo"$waktu1 hari = $jumlahTenagaKerja pekerja <br>";
+echo"$waktu2 hari = $jumlahTenagaKerja2 pekerja <br>";
+echo"x = $waktu2";
+?>
---
+++ tugas/tugas2_2.php
@@ -0,0 +1,10 @@
+<?php
+$jual = 80000;
+$beli = 50000;
+
+$keuntungan =(($jual-$beli)/$beli)*100;
+
+echo"jual = $jual<br>";
+echo"beli = $beli <br>";
+echo"keuntungan = $keuntungan %";
+?>
